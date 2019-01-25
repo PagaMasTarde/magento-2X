9089afc2517176e54a1d41d5eb3fc64101eda9a7
romeritoCL <romero.latorre@gmail.com>
2019-01-25 10:31:31 +0100

    Updated composer json

--- docker-init.sh
+++ docker-init.sh
@@ -41,8 +41,6 @@ else
         package='dev-master'
     fi
 
-    package='v7.0.6.x-dev'
-
     echo 'Package: '$package
     docker-compose exec -u www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento cache:enable
     docker-compose exec -u www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento deploy:mode:set production
